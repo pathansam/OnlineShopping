7bb6e73b9f0f4872917f105fa52225fd9e2d4c16
sameer <pathansam045@gmail.com>
2022-07-23 15:41:34 +0530

    del 160.2

--- day16.2/src/main/java/com/app/dao/IUserDao.java
+++
@@ -1,7 +0,0 @@
-package com.app.dao;
-
-import com.app.pojos.User;
-
-public interface IUserDao {
-	User validateUser(String email, String password);
-}
--- day16.2/src/main/java/com/app/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.app.service;
-
-import com.app.pojos.User;
-
-public interface IUserService {
-	User authenticateUser(String em, String pass);
-}
